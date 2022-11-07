1f243d02c2e255ca6eacc531bbd5becffdfc8fd6
maxpel-dev <maxime.peloutier@ymail.com>
2022-11-08 00:39:47 +0100

    Avancement sur les satellites

--- api.py
+++ api.py
@@ -1,18 +1,33 @@
 import json
 import requests
 
-def GetCelestailBodyData(name):
-    api_url = 'https://api.le-systeme-solaire.net/rest.php/bodies/{}'.format(name)
+# [!] Lancez plutôt main.py pour démarrer la simulation !
+# Voir https://api.le-systeme-solaire.net/swagger/ pour les modèles de données reçues
+
+#Utilisé pour les planètes et le soleil
+def GetCelestialBodyDataFromId(id):
+    api_url = 'https://api.le-systeme-solaire.net/rest.php/bodies/{}'.format(id)
     response = requests.get(api_url)
     if response.status_code == requests.codes.ok:
-        #print(response.text)
-        print(name + " chargé(e)")
+        print(response.json()["name"] + " chargé(e)")
     else:
         print("Error:", response.status_code, response.text)
     return response.json()
 
-response = GetCelestailBodyData("Mercury")
+# 
+def GetCelestialBodyDataFromUrl(url):
+    response = requests.get(url)
+    if response.status_code == requests.codes.ok:
+        print(response.json()["name"] + " chargé(e)")
+    else:
+        print("Error:", response.status_code, response.text)
+    return response.json()
 
+# Seulement du code de test à partir de ce commentaire
+response = GetCelestialBodyDataFromId("Mars")
 print(json.dumps(response, indent=2))
-
-
+moons = response["moons"]
+print(json.dumps(moons, indent=2))
+firstMoonUrl = moons[0]["rel"]
+moonData = GetCelestialBodyDataFromUrl(firstMoonUrl)
+print(json.dumps(moonData, indent=2))
--- main.py
+++ main.py
@@ -6,12 +6,14 @@ pygame.init()
 w,h = 1520, 768
 screen = pygame.display.set_mode((w,h))
 displayPlanetOrbits = True
+ENABLE_SATELLITES = False
 
+BASE_URL = "https://api.le-systeme-solaire.net/rest.php/bodies/"
 SUN_X, SUN_Y = 230, h/2
 SUN_COLOR = (235, 158, 26)
-# Echelles - Vous pouvez vous amuser à jouer avec, je pense avoir trouvé un bon équilibre entre précision et qualité d'affichage
+# Echelles - Vous pouvez vous amuser à jouer avec, je pense avoir trouvé un bon équilibre entre précision et lisibilité d'affichage
 DISTANCE_SCALE = 1/1600
-SUN_SCALE = 1/5000
+SUN_SCALE = 1/5300
 PLANET_SCALE = SUN_SCALE*6
 SATELLITE_SCALE = 1/5000
 PLANET_ORBIT_OFFSET = 42/SUN_SCALE # Distance fixe ajoutée à la distance entre le soleil et chaque planète pour éviter que les planètes proches du soleil se fassent "manger"
@@ -19,7 +21,7 @@ PLANET_ORBIT_OFFSET = 42/SUN_SCALE # Distance fixe ajoutée à la distance entre
 SPEED_MULTIPLIERS = [0.5, 1, 2, 5, 10, 50, 100, 250, 500, 1000, 5000]
 speedMultiplierIndex = 4
 FRAMERATE = 165
-# Nom et couleurs des planètes du système solaire à inclure dans la simulation
+# ID (peut être différent du NOM, mais pas le cas ici) et couleurs des planètes du système solaire à inclure dans la simulation
 PLANETS = [ 
     ("Mercury", (240, 198, 144)),
     ("Venus", (237, 146, 26)),
@@ -31,51 +33,54 @@ PLANETS = [
     ("Neptune", (11, 67, 179))
     # Désolé Pluton, t'es pas tout à fait une planète et puis ça fait trop loin :(
 ]
+SATELLITE_COLOR = (204, 204, 204)
 
-# Les valeurs des attributs sont ramenées aux échelles définies dans les constantes.
 class CelestialBody:
-    def __init__(self, name, color):
-        self.name = name
+    """
+    Considérez cette classe comme abstraite. Il s'agît de la classe de base des différents corps célestes affichés dans la simulation.\n
+    """
+    def __init__(self, id, color, initial_x = w/2, initial_y = h/2):
+        self.x = initial_x
+        self.y = initial_y
+        self.id = id
         self.color = color
-        data = api.GetCelestailBodyData(self.name)
-
-        # Distance du soleil : "semimajorAxis"
-        # Rayon : "meanRadius"
-        # Jours par orbite = "sideralOrbit"
-        # Jours par rotation sur soi-même = "sideralRotation" (inutilisé actuellement)
-        self.daysPerOrbit = data["sideralOrbit"]
-        self.distanceFromOrbitCenter = data["semimajorAxis"]*DISTANCE_SCALE + PLANET_ORBIT_OFFSET
-        self.radius = data["meanRadius"]*PLANET_SCALE
-        self.updatePosition()
-        print(str(self))
+        # Les données de la réponse API sont temporairement stockées dans un attribut de l'objet
+        # Pour que les objets des classes héritant de CelestailBody puissent y accéder en appelant le constructeur supérieur
+        self.apiData = api.GetCelestialBodyDataFromId(self.id)
+        self.radius = self.apiData["meanRadius"] # Les échelles sont appliquées dans les constructeurs des classes inférieures
+        #print(str(self))
+        self.draw()
     
     def __str__(self):
         s = ""
-        s += "Nom : " + str(self.name) + "\n"
+        s += "ID : " + str(self.id) + "\n"
         s += "Coordonnées (x, y) : " + str(self.x) + ", " + str(self.y) + "\n"
         return s
-
-    def refresh(self):
-        self.draw()
-
-    def updatePosition(self, parent_x = SUN_X, parent_y = SUN_Y):
-        self.orbitCount += (SPEED_MULTIPLIERS[speedMultiplierIndex]/FRAMERATE)/self.daysPerOrbit
-        self.angle = (2*math.pi)*self.orbitCount
-        self.x = (self.distanceFromOrbitCenter*math.cos(self.angle))*DISTANCE_SCALE + parent_x
-        self.y = (self.distanceFromOrbitCenter*math.sin(self.angle))*DISTANCE_SCALE + parent_y
     
     def draw(self):
         # print(str(self))
         pygame.draw.circle(screen, self.color, (self.x, self.y), self.radius)
 
-class Planet(CelestialBody):
-    def __init__(self, name, color):
+
+
+class OrbitingBody(CelestialBody):
+    """
+    Considérez cette classe comme abstraite. Planet et Satellite héritent directement de cette classe.\n
+    Ce type de corps céleste se base sur une référence (de type Sun ou Planet) autour de laquelle celui-ci orbite pour déterminer sa position.
+    """
+    def __init__(self, id, color, orbitRef):
         self.angle = 0
         self.orbitCount = 0
-        super().__init__(name, color)
-        self.satellites = []
-        # TODO ajouter les satellites à chaque planète
-
+        self.orbitReference = orbitRef
+        super().__init__(id, color)
+        # Distance du soleil : "semimajorAxis"
+        # Rayon : "meanRadius"
+        # Jours par orbite = "sideralOrbit"
+        # Jours par rotation sur soi-même = "sideralRotation" (inutilisé actuellement)
+        self.daysPerOrbit = self.apiData["sideralOrbit"]
+        self.distanceFromOrbitCenter = self.apiData["semimajorAxis"]*DISTANCE_SCALE
+        self.refresh()
+    
     def __str__(self):
         s = super().__str__()
         s += "Nombre d'orbites : " + str(self.orbitCount) + "\n"
@@ -86,35 +91,48 @@ class Planet(CelestialBody):
         self.updatePosition()
         if displayPlanetOrbits:
             self.drawOrbit()
-        super().draw()
+        self.draw()
 
-    def updatePosition(self, relative_x = SUN_X, relative_y = SUN_Y):
-        self.orbitCount += (SPEED_MULTIPLIERS[speedMultiplierIndex]/FRAMERATE)/self.daysPerOrbit
-        self.angle = (2*math.pi)*self.orbitCount
-        self.x = (self.distanceFromOrbitCenter*math.cos(self.angle))*DISTANCE_SCALE + relative_x
-        self.y = (self.distanceFromOrbitCenter*math.sin(self.angle))*DISTANCE_SCALE + relative_y
+    def updatePosition(self):
+        if(self.daysPerOrbit != 0):
+            self.orbitCount += (SPEED_MULTIPLIERS[speedMultiplierIndex]/FRAMERATE)/self.daysPerOrbit
+            self.angle = (2*math.pi)*self.orbitCount
+            self.x = (self.distanceFromOrbitCenter*math.cos(self.angle))*DISTANCE_SCALE + self.orbitReference.x
+            self.y = (self.distanceFromOrbitCenter*math.sin(self.angle))*DISTANCE_SCALE + self.orbitReference.y
 
     def drawOrbit(self):
-        pygame.draw.circle(screen, (255, 255, 255), (SUN_X, SUN_Y), self.distanceFromOrbitCenter*DISTANCE_SCALE, 1)
+        pygame.draw.circle(screen, (255, 255, 255), (self.orbitReference.x, self.orbitReference.y), self.distanceFromOrbitCenter*DISTANCE_SCALE, 1)
+
+class Planet(OrbitingBody):
+    def __init__(self, id, color, orbitRef):
+        super().__init__(id, color, orbitRef)
+        self.radius *= PLANET_SCALE
+        self.distanceFromOrbitCenter += PLANET_ORBIT_OFFSET
+        # Récupération des satellites
+        satelliteInfo = self.apiData["moons"]
+        self.satellites = []
+        if (satelliteInfo is not None and ENABLE_SATELLITES == True): # Une planète peut ne pas avoir de satellite, comme par exemple Mercure
+            for s in satelliteInfo:
+                satId = s["rel"].rsplit('/', 1)[-1] # Récupère le dernier mot de l'url pour avoir l'ID du satellite (voir explication dans api.py)
+                self.satellites.append(Satellite(satId, SATELLITE_COLOR, self)) 
+        self.radius *= PLANET_SCALE
+
+class Satellite(OrbitingBody):
+    def __init__(self, id, color, orbitRef):
+        super().__init__(id, color, orbitRef)
+        self.radius *= SATELLITE_SCALE
 
 class Sun(CelestialBody):
+    """
+    Corps de référence de la simulation, il est considéré comme immobile.\n
+    """
     def __init__(self):
-        self.name = "Sun"
-        self.color = SUN_COLOR
-        data = api.GetCelestailBodyData(self.name)
-        self.radius = data["meanRadius"]*SUN_SCALE
+        super().__init__("Sun", SUN_COLOR, SUN_X, SUN_Y)
+        self.radius *= SUN_SCALE
         self.planets = []
         for p in PLANETS:
-            self.planets.append(Planet(p[0], p[1]))
-        self.draw()
-
-    # La simulation étant héliocentrique, pas besoin de recalculer la position du soleil
-    def refresh(self):
-        self.draw()
-    
-    def draw(self):
-        pygame.draw.circle(screen, SUN_COLOR, (SUN_X, SUN_Y), self.radius)
-
+            self.planets.append(Planet(p[0], p[1], self))
+        super().draw()
 
 
 play = True
@@ -150,7 +168,7 @@ while play:
     # Rafraîchissement de l'affichage du système solaire
     for planet in sun.planets:
         planet.refresh()
-    sun.refresh()
+    sun.draw()
 
     clock.tick(FRAMERATE)
     pygame.display.flip()
